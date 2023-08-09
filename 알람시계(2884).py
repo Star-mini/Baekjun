feb3f581dcd1d00c68f5ff98dd3bdce923d46093
Park <minisstar@naver.com>
2023-08-10 03:51:46 +0900

    2884하는중

---
+++ 알람시계(2884).py
@@ -0,0 +1,15 @@
+a,b=input().split()
+
+a=int(a)
+b=int(b)
+
+if a==0:
+    if b>=45:
+        print(0,b-45)
+    else:
+        print(23,b-45+60)
+else:
+    if b>=45:
+        print(a-1,b-45)
+    else:
+        print(a-1,b-45+60)
